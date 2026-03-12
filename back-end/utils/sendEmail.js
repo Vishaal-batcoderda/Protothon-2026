6e15569f20875043142d7ee0ed8b15881b8714c1
Vishaal Ravichandran <vishaalravichandran369@gmail.com>
2026-03-12 23:23:54 +0530

    Update sendEmail.js

--- back-end/utils/sendEmail.js
+++ back-end/utils/sendEmail.js
@@ -8,15 +8,35 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+/**
+ * Send Email Utility
+ * @param {string | string[]} to - recipient email or array of emails
+ * @param {string} subject - email subject
+ * @param {string} html - html email body
+ */
+
 const sendEmail = async (to, subject, html) => {
+  try {
 
-  await transporter.sendMail({
-    from: `"Protothon 2026" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html
-  });
+    const mailOptions = {
+      from: `"Protothon 2026" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html
+    };
 
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log("Email sent successfully ✅");
+    console.log("Message ID:", info.messageId);
+
+  } catch (error) {
+
+    console.error("Email sending failed ❌");
+    console.error(error);
+
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
